1ac714ad91fa98db06a46cc4c09e1aba0b920391
karenbarseghyan <karenbarseghyan90@gmail.com>
2021-07-30 00:02:03 +0400

    Importing BodyParser

--- index.js
+++ index.js
@@ -1,11 +1,15 @@
-const express = require('express')
+// const express = require('express')
+import express from 'express';
+import bodyParser from 'body-parser';
+
 const app = express();
 const port = 3000;
 
-app.get('/', (req,res) => {
+/*app.get('/', (req,res) => {
     res.end("Hello Everyone")
-})
+})*/
 
+app.use(bodyParser.json())
 app.listen(3000, () => {
     console.log(`Listening at http://localhost:${port}`)
 })
