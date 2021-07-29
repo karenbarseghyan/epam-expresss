c0aa1ecf3dc3d5a53a306293219b17ca340bdc1a
karenbarseghyan <karenbarseghyan90@gmail.com>
2021-07-30 01:56:32 +0400

    POST & GET request - getting a specific user with ID

--- routes/users.js
+++ routes/users.js
@@ -1,19 +1,9 @@
 import express from 'express';
+import { v4 as uuidv4 } from 'uuid';
 
 const router = express.Router();
 
-const users = [
-    {
-        name: "Karen",
-        surname: "Barseghyan",
-        major: "NodeJS"
-    },
-    {
-        name: "Leo",
-        surname: "Barseghyan",
-        major: "none"
-    }
-]
+let users = []
 
 router.get('/', (req,res) => {
     console.log(users);
@@ -21,12 +11,21 @@ router.get('/', (req,res) => {
 })
 
 router.post('/', (req,res) => {
-    console.log("req reached");
-    console.log(req.body);
     const user = req.body;
-    users.push(user);
+    // const userId = uuidv4();
+    // const userWithId = {...user, id: uuidv4()}
+
+    users.push({...user, id: uuidv4()});
     res.send(`user <<${user.name}>> added`);
 
+})
+
+router.get('/:id', (req, res) => {
+    // console.log(req.params.id);
+    // const id = req.params.id;
+    const {id} = req.params;
+    const foundUser = users.find((user) => user.id === id)
+    res.send(foundUser);
 })
 
 export default router;
