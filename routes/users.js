f2836345410d5ac057c7cf73bed7359260640f7e
karenbarseghyan <karenbarseghyan90@gmail.com>
2021-07-30 01:20:06 +0400

    POST request

--- routes/users.js
+++ routes/users.js
@@ -2,8 +2,31 @@ import express from 'express';
 
 const router = express.Router();
 
+const users = [
+    {
+        name: "Karen",
+        surname: "Barseghyan",
+        major: "NodeJS"
+    },
+    {
+        name: "Leo",
+        surname: "Barseghyan",
+        major: "none"
+    }
+]
+
 router.get('/', (req,res) => {
-    res.send("Users Page");
+    console.log(users);
+    res.send(users);
+})
+
+router.post('/', (req,res) => {
+    console.log("req reached");
+    console.log(req.body);
+    const user = req.body;
+    users.push(user);
+    res.send(`user <<${user.name}>> added`);
+
 })
 
 export default router;
